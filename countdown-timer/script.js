a0210ef09db4f26a4941f1fcdde7492f60c85e53
LAPTOP-VPDI9AOJ\namla <leanhnam94@gmail.com>
2021-05-27 17:18:29 +0700

    Add countdown projects

---
+++ countdown-timer/script.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYearsEve = '1 Jan 2022';
+
+function countDown() {
+    const newYearsDate = new Date(newYearsEve);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    console.log(days, hours, mins, seconds);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10? `0${time}` : time;
+}
+
+setInterval(countDown, 1000);
