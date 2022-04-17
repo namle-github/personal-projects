48fa49bf637e7c6a1a5011357c7190c55bd17381
leanhnam <leanhnam94@gmail.com>
2022-04-17 17:10:45 +0700

    Update new year getter

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,7 +3,11 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
-const newYearsEve = '1 Jan 2023';
+// Parse new year's eve date
+const currentDate = new Date();
+const newYear = currentDate.getFullYear() + 1
+const newYearsEve = '1 Jan ' + newYear;
+console.log("new year:", newYearsEve)
 
 function countDown() {
     const newYearsDate = new Date(newYearsEve);
